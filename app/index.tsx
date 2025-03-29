db0e7398ad0c803c522aae7f718414337a985ecc
RayAnthoney <rmaxey911@gmail.com>
2025-03-29 11:58:53 -0600

    First commit for my native movie app

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-50">
+      <Text className="text-5xl text-accent font-bold">Welcome!</Text>
+    </View>
+  );
+}
