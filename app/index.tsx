9afea6cf75688deb0ebbe496aced9bb9f66e3a66
RayAnthoney <rmaxey911@gmail.com>
2025-03-30 12:16:00 -0600

    replace assets folder

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-50" >
-      <Text className="text-5xl text-accent font-bold">Welcome!</Text>
-    </View>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
     extend: {
       colors: {
         primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#22qf39",
+          200: "#0f0d23",
+        },
         accent: "#AB8BFF",
-      }
+      },
     },
   },
   plugins: [],
-}
-
+};
