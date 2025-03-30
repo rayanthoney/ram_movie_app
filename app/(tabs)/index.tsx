212b2714a66c9a91c69971e90a74b7a01086f5ed
RayAnthoney <rmaxey911@gmail.com>
2025-03-30 12:10:48 -0600

    created the Tabs section for the menu

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-50" >
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>      
+    </View>
+  );
+}
