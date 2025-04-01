a1012af098836ea373677bc6bfae953da4b2c45a
RayAnthoney <rmaxey911@gmail.com>
2025-04-01 15:02:47 -0600

    Created useFetch hook for popular movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,25 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 import { useRouter } from "expo-router";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+
 
 
 
 export default function Index() {
   const router = useRouter();
+
+  const { 
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: "" 
+  }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg}  className="absolute w-full z-0" />
@@ -15,12 +27,26 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
+        {moviesLoading ? (
+          <ActivityIndicator 
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center" 
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
         <View className="flex-1 mt-5">
           <SearchBar
             onPress={() => router.push("/search")}
             placeholder="Search for a movie"
           />
+
+          <>
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+          </>
         </View>
+        )}
       </ScrollView>
     </View>
   );
