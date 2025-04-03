fc24f936de6b3fa92c839236deb65cbf5382f803
RayAnthoney <rmaxey911@gmail.com>
2025-04-03 06:09:36 -0600

    Created the MovieCard to display popular movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({
   id,
@@ -11,7 +12,7 @@ const MovieCard = ({
 }: Movie) => {
   console.log(poster_path);
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -23,7 +24,21 @@ const MovieCard = ({
           resizeMode="cover"
         />
 
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>{title}</Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold upperrcase">{Math.round(vote_average / 2)}</Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+          {/* <Text className="text-xs text-light-300 font-medium uppercase">
+            Movie
+          </Text> */}
+        </View>
       </TouchableOpacity>
     </Link>
   );
