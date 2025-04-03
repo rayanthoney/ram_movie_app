12f86c651232efc3b394c8820248e898392f5049
RayAnthoney <rmaxey911@gmail.com>
2025-04-03 10:18:00 -0600

    started the setup of the Search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { icons } from "@/constants/icons";
+import { SearchBar } from "react-native-screens";
 
 const search = () => {
-  return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default search
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+  return (
+    <View className="flex-1 bg-primary ">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+              />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+          </>
+        }
+      />
+    </View>
+  );
+};
+
+export default search;
