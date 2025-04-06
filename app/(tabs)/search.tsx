b6bc549c76bb05d4a00e2aa060ee5ada00a94e15
RayAnthoney <rmaxey911@gmail.com>
2025-04-06 09:55:15 -0600

    Create appwrite database

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import MovieCard from "@/components/MovieCard";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useEffect, useState } from "react";
+import { updateSearchCount } from "@/services/appwrite";
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -19,17 +20,18 @@ const Search = () => {
   } = useFetch(() => fetchMovies({ query: searchQuery }), false); // Recheck this line UI dissapears
 
   // Debounced search effect
-    useEffect(() => {
-      const timeoutId = setTimeout(async () => {
-    if (searchQuery.trim()) {
-     await loadMovies()
-    } else {
-      reset();
-    }
-  }, 500);
+  useEffect(() => {
+    updateSearchCount(searchQuery, movies[0]);
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
 
-  return () => clearTimeout(timeoutId);
-    }, [searchQuery]);
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
 
   return (
     <View className="flex-1 bg-primary ">
@@ -58,8 +60,8 @@ const Search = () => {
             </View>
 
             <View>
-              <SearchBar 
-                placeholder="Search movies..." 
+              <SearchBar
+                placeholder="Search movies..."
                 value={searchQuery}
                 onChangeText={(text: string) => setSearchQuery(text)}
               />
@@ -79,25 +81,22 @@ const Search = () => {
               </Text>
             )}
 
-            {!loading &&
-              !error &&
-              searchQuery.trim() &&
-              movies?.length > 0 && (
-                <Text className="text-xl text-white font-bold mt-5 mb-3 px-5">
-                  Search Results for {" "}
-                  <Text className="text-accent">{searchQuery}</Text>
-                </Text>
-              )}
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold mt-5 mb-3 px-5">
+                Search Results for{" "}
+                <Text className="text-accent">{searchQuery}</Text>
+              </Text>
+            )}
           </>
         }
         ListEmptyComponent={
           !loading && !error ? (
             <View>
               <Text className="text-center text-gray-500">
-                {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
+                {searchQuery.trim() ? "No movies found" : "Search for a movie"}
               </Text>
             </View>
-        ) : null
+          ) : null
         }
       />
     </View>
