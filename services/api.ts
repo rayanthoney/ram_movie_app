2cb0ed8cffa7ac0f9df6e0efc65f948388446c0d
RayAnthoney <rmaxey911@gmail.com>
2025-04-09 06:24:38 -0600

    created api for the MovieDetails

--- services/api.ts
+++ services/api.ts
@@ -10,22 +10,46 @@ export const TMBD_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-     ? `${TMBD_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-     : `${TMBD_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMBD_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMBD_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
-  const response = await fetch(endpoint, { 
+  const response = await fetch(endpoint, {
     method: "GET",
     headers: TMBD_CONFIG.headers,
   });
 
-    if(!response.ok) {
-        // @ts-ignore 
-        throw new Error('Failed to fetch movies', response.statusText);
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMBD_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMBD_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMBD_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details`);
     }
 
     const data = await response.json();
-
-    return data.results;
+    return data;
+    
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
-
 // /discover/movie
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,56 +1,63 @@
-import {Client, Databases, ID, Query} from "react-native-appwrite";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-    .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!); // Your project ID
+  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!); // Your project ID
 
 const database = new Databases(client);
-  
+
 export const updateSearchCount = async (query: string, movie: Movie) => {
-  try {  
-  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-    Query.equal('searchTerm', query )
-  ])
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
 
-  //  check if a record if that search has already been stored 
-  if (result.documents.length > 0) {
-    const existingMovie = result.documents[0];
+    //  check if a record if that search has already been stored
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
 
-    await database.updateDocument(
-      DATABASE_ID,
-      COLLECTION_ID,
-      existingMovie.$id,
-      {
-        count: existingMovie.searchCount + 1,
-      }
-    )
-  } else {
-    await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-      searchTerm: query,
-      movie_id: movie.id,
-      count: 1,
-      title: movie.title,
-      poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`, 
-    })
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.searchCount + 1,
+        }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        count: 1,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-} catch (error) {
-  console.log(error);
-  throw error;
-}
 
   // if a document is found increment the searchCount field
   // if no document is found
   // create a new document in Appwrite database --> 1
 };
 
-export const getSearchCount = async (): Promise<TrendingMovie[] | undefined > => {
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
   try {
-    
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
   } catch (error) {
     console.log(error);
-    throw error;
+    return undefined;
   }
-}
+};
