c8ad15afe2f6019b68c500a015299fb91e17eed3
RayAnthoney <rmaxey911@gmail.com>
2025-04-10 09:15:23 -0600

    created api for the MovieDetails

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
 
 const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmbd.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
