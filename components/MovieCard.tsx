f59ac2cc8fd05064b9fc4293d5dbead33e237b5e
RayAnthoney <rmaxey911@gmail.com>
2025-04-27 08:51:37 -0600

    refactored  movieDetails

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,13 +3,7 @@ import React from "react";
 import { Link } from "expo-router";
 import { icons } from "@/constants/icons";
 
-const MovieCard = ({
-  id,
-  poster_path,
-  title,
-  vote_average,
-  release_date,
-}: Movie) => {
+const MovieCard = ({ id, poster_path, title, vote_average, release_date, }: Movie) => {
   console.log(poster_path);
   return (
     <Link href={`/movies/${id}`} asChild>
@@ -27,17 +21,19 @@ const MovieCard = ({
         <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>{title}</Text>
 
         <View className="flex-row items-center justify-start gap-x-1">
-          <Image source={icons.star} className="size-4" />
+          <Image source={icons.star} className="size-4"/>
           <Text className="text-xs text-white font-bold upperrcase">{Math.round(vote_average / 2)}</Text>
-        </View>
 
-        <View className="flex-row items-center justify-between">
-          <Text className="text-xs text-light-300 font-medium mt-1">
-            {release_date?.split("-")[0]}
-          </Text>
-          {/* <Text className="text-xs text-light-300 font-medium uppercase">
-            Movie
-          </Text> */}
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xs text-light-300 font-medium mt-1">
+              {release_date?.split("-")[0]}
+            </Text>
+
+            {/* <Text className="text-xs font-medium text-light-300 uppercase">
+              Movie
+            </Text> */}
+            
+          </View>
         </View>
       </TouchableOpacity>
     </Link>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,32 +1,27 @@
-import { View, Image, TextInput } from "react-native";
-import { icons } from "@/constants/icons";
+import { View, Text, Image, TextInput } from 'react-native'
+import { icons } from '@/constants/icons'
 
 interface Props {
-  placeholder: string;
-  value?: string;
-  onChangeText?: (text: string) => void; 
-  onPress?: () => void;
+    placeholder: string;
+    value?: string;
+    onPress?: () => void;
+    onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-      <Image
-        source={icons.search}
-        className="size-5"
-        resizeMode="contain"
-        tintColor="#AB8BFF"
-      />
-      <TextInput
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+      <TextInput 
         onPress={onPress}
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
-        placeholderTextColor={"#A8B5DB"}
-        className="flex-1 ml-2 text-white"
+        placeholderTextColor={"#a8b5db"}
+        className='flex-1 ml-2 text-white'
       />
     </View>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
